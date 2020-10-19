1e67ab77691eda3068dbadac1ab6b851f5224658
Anurag Madhesia <60473538+anuragmadhesia@users.noreply.github.com>
2020-10-19 09:00:53 +0530

    added models

---
+++ MyWebsite/Models/CapthaCodeGenerator.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MyWebsite.Models
+{
+    public class CapthaCodeGenerator
+    {
+        public string GetCaptcha()
+        {
+            char ch1, ch2, ch3, ch4, ch5;
+            Random rm = new Random();
+            ch1 = (char)(rm.Next(65,90));
+            ch2 = (char)(rm.Next(50,55));
+            ch3 = (char)(rm.Next(100,122));
+            ch4 = (char)(rm.Next(50,55));
+            ch5 = (char)(rm.Next(50,54));
+            string cph=ch1 + "" + ch2 + "" + ch3 + "" + ch4 + "" + ch5 +"";
+            return cph;
+        }
+    }
+}
---
+++ MyWebsite/Models/ConnectionManager.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data;
+using System.Data.SqlClient;
+using System.IO;
+
+namespace MyWebsite.Models
+{
+    public class ConnectionManager
+    {
+        SqlConnection con = null;
+        SqlCommand cmd = null;
+        public ConnectionManager()
+        {
+            con = new SqlConnection(@"Data Source=AMAN\SQL;Initial Catalog=CityRestaurant;Integrated Security=True");
+        }
+        public bool MyInsertUpdateDelete(string command)
+        {
+            cmd = new SqlCommand(command,con);
+            if (con.State == ConnectionState.Closed)
+            {
+                con.Open();
+            }
+            int n = cmd.ExecuteNonQuery();
+            if (n > 0)
+                return true;
+            else
+                return false;
+        }
+        public DataTable DisplayAllData(string command)
+        {
+            DataTable dt = new DataTable();
+            SqlDataAdapter sa = new SqlDataAdapter(command, con);
+            sa.Fill(dt);
+            return dt;
+        }
+    }
+}
